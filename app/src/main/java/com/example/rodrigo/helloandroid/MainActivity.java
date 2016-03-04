622a4931f6f96a6d252f9c4f9d9e198babc96533
RodrigoManfrini <rmanfrini@gmail.com>
2016-03-04 16:49:32 -0300

    Initial commit

---
+++ app/src/main/java/com/example/rodrigo/helloandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.rodrigo.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by Rodrigo on 01/03/2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
